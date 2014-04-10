119901fa55d458c2a2faf7cab4148820c114fe8c
meiskam <meiskam@gmail.com>
2014-04-10 01:10:36 -0400

    Updated version to 1.3.8.1

--- src/net/ftb/gui/LaunchFrame.java
+++ src/net/ftb/gui/LaunchFrame.java
@@ -141,7 +141,7 @@ public class LaunchFrame extends JFrame {
     private static String[] dropdown_ = { "Select Profile", "Create Profile" };
     private static JComboBox users, tpInstallLocation, mapInstallLocation;
     private static LaunchFrame instance = null;
-    private static String version = "1.3.8";
+    private static String version = "1.3.8.1";
     public static boolean canUseAuthlib;
 
     public final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
@@ -153,7 +153,7 @@ public class LaunchFrame extends JFrame {
     public TexturepackPane tpPane;
     public OptionsPane optionsPane;
 
-    public static int buildNumber = 138;
+    public static int buildNumber = 1381;
     public static boolean noConfig = false;
     public static boolean allowVersionChange = false;
     public static boolean doVersionBackup = false;
